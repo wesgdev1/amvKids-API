972bb8437d69d635feb83fe600f9b795e617261f
Welinton Suarez <wesgdeveloper@gmail.com>
2024-05-28 10:08:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AMV KIDS
+
+AMV KIDS API es un servicio web desarrollado en Node.js y Express.js
+
+## Instalación
+
+Sigue estos pasos para configurar el proyecto en tu entorno de desarrollo:
+
+1. Clona este repositorio:
---
+++ index.js
@@ -0,0 +1,16 @@
+import http from "http";
+import { configuration } from "./app/config.js";
+import { app } from "./app/index.js";
+import { connect } from "./app/database.js";
+
+const { port } = configuration.server;
+
+// Connect to database
+connect();
+
+// Create server
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running at ${port} port`);
+});
