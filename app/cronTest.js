20c9a171c9e20608ad05ecedb1ebc0f4b18e2d72
Welinton Suarez <wesgdeveloper@gmail.com>
2025-05-06 10:29:54 -0500

    feat: Add cron job script for scheduled tasks execution

---
+++ app/cronTest.js
@@ -0,0 +1,10 @@
+// Schedule the task to run at 8 PM every day
+const runCronJob = async () => {
+  try {
+    console.log("hola desde a la hora", new Date().toLocaleString());
+
+    console.log("Cron job executed successfully:", result);
+  } catch (error) {
+    console.error("Error executing cron job:", error);
+  }
+};
